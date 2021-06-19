fba1840d7e3968eb95df43d9d318ef80704afb4f
Pai Qu <pai.qu@outlook.com>
2021-06-19 15:49:40 +1000

    added basic form fields

--- src/components/CardForm.tsx
+++ src/components/CardForm.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 import Card from "@repay/react-credit-card";
+import "@repay/react-credit-card/dist/react-credit-card.css";
+
 
 function CardForm() {
   const [formData, setFormData] = useState({
@@ -28,6 +30,42 @@ function CardForm() {
           value={formData.name}
         />
       </fieldset>
+      <fieldset>
+        <legend>Phone Number</legend>
+        <input
+          name='phone'
+          onChange={e => handleChange('phone', e)}
+          value={formData.phone}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Card Number</legend>
+        <input
+          name='number'
+          placeholder="xxxx xxxx xxxx xxxx"
+          onChange={e => handleChange('number', e)}
+          value={formData.number}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>Expiration Date</legend>
+        <input
+          name='expiry'
+          placeholder="MM/YY"
+          onChange={e => handleChange('expiry', e)}
+          value={formData.expiry}
+        />
+      </fieldset>
+      <fieldset>
+        <legend>cvv</legend>
+        <input
+          name='cvv'
+          placeholder="xxx"
+          onChange={e => handleChange('cvv', e)}
+          value={formData.cvv}
+        />
+      </fieldset>
+      <Card {...formData} />
     </div>
   );
 }
